5d6926aef2b06dd7495006d7f737b72e5dc1ea71
Kenokuda <xiandoua@gmail.com>
2025-04-17 06:12:41 +0900

    add: フォーマッターの設定 add: 各ページの追加

--- my-portfolio/pages/index.vue
+++ my-portfolio/pages/index.vue
@@ -1,7 +1,20 @@
-<script lang="ts" setup>
-//
-</script>
-
 <template>
-  <HelloWorld />
+  <v-row v-for="(page, index) in pages" :key="index">
+    <v-col>
+      <v-card>
+        <v-card-title>{{ page.name }}</v-card-title>
+        <v-card-actions><v-btn></v-btn></v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
+
+<script setup lang="ts">
+const pages = [
+  { name: "私について", to: "/about" },
+  { name: "ブログ", to: "/about" },
+  { name: "連絡", to: "/about" },
+  { name: "技術", to: "/about" },
+  { name: "プロジェクト一覧", to: "/about" },
+];
+</script>
