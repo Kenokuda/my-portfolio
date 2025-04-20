ce181a20cc9a1f858cf623115fe31f4fb6a9d184
Kenokuda <xiandoua@gmail.com>
2025-04-20 14:16:06 +0900

    fix: urlを設定

--- my-portfolio/nuxt.config.ts
+++ my-portfolio/nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
 
+  app: {
+    baseURL: "/my-portfolio/", // GitHub Pages のサブパスに合わせる
+  },
+
   // github pages deployment
   nitro: {
     preset: "static",
